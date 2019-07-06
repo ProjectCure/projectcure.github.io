618d7770b53e06ab81292ce27320342625812015
David Muniz <david.munizmacias@pop.belmont.edu>
2019-07-06 08:11:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -9,17 +9,14 @@ $(document).ready(function() {
         $.getJSON('data.json',function(data) {
             $.each(data, function(key, value) {
               //search bias
-                // if (value.manufacture.search(expression) != -1 || value.Model.search(expression) != -1)
-                if (value.Model.search(expression) != -1)
+                if (value.manufacture.search(expression) != -1 || value.Model.search(expression) != -1)
                 {
 
 
 
 
 
-                  // $('#result').append('<li class="list-group-item link-class"><a href=" '+value.manuelurl+' " class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>'+value.manufacture+' | <span class="text-muted">'+value.Model+'</span></li>');
-                  $('#result').append('<li class="list-group-item link-class"><a href=" '+value.Model+' " class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">'+value.Model+'</a></li>');
-
+                  $('#result').append('<li class="list-group-item link-class"><a href=" '+value.manuelurl+' " class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>'+value.manufacture+' | <span class="text-muted">'+value.Model+'</span></li>');
                 }
 
 
@@ -32,11 +29,15 @@ $(document).ready(function() {
 
 
 });
-$('#result').on('click', function() {
-  alert($(this).html());
-  // x = $(this).text();
-  //
-  // titleTest = titleString.replace(/\"/g, "");
+// $('#result').on('click', function() {
+//   alert($(this).text());
+//
+//
+//
+//
+// });
+$('#result li:selected').text();
+alert($('#result li:selected').text());
 
 
 
