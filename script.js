f0ab17c51b87b76ee4dc4905b3aac767858dee5f
David Muniz <david.munizmacias@pop.belmont.edu>
2019-07-06 17:04:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 
 
 
-                  $('#result').append('<li class="list-group-item link-class"><a href=" '+value.manuelurl+' " class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>'+value.manufacture+' | <span class="text-muted">'+value.Model+'</span></li>');
+                  $('#result').append('<li class="list-group-item link-class"><a href=" '+value.manuelurl+' " class="btn btn-secondary btn-lg active" role="button" aria-pressed="false">Link</a>'+value.manufacture+' | <span class="text-muted">'+value.Model+'</span></li>');
                 }
 
 
