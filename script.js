2a67f0377d4033ff16a75c76c5be56cd93b34c98
David Muniz <david.munizmacias@pop.belmont.edu>
2019-07-06 01:34:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+<!-- Jquery -->
+$(document).ready(function() {
+    $('#search').keyup(function(event) {
+        // $.ajaxSetup({ cache: false });
+        $('#result').html('');
+        var searchField = $('#search').val();
+        var expression = new RegExp(searchField, "i");
+        $.getJSON('data.json',function(data) {
+            $.each(data, function(key, value) {
+              //search bias
+                if (value.manufacture.search(expression) != -1 || value.Model.search(expression) != -1)
+                {
+
+
+
+
+
+                  $('#result').append('<li class="list-group-item link-class"><a href=" '+value.manuelurl+' " class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Link</a>'+value.manufacture+' | <span class="text-muted">'+value.Model+'</span></li>');
+                }
+
+
+
+
+
+
+        });
+    });
+});
+});
