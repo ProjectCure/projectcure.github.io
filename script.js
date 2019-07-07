790624f8f01a87d4aeb457fee7b988385a032365
David Muniz <david.munizmacias@pop.belmont.edu>
2019-07-07 06:49:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
                   //need to reduce this to just get the name
                   // $('#result').append('<li class="list-group-item link-class"><a href=" '+value.manuelurl+' " class="btn btn-secondary btn-lg" role="button" aria-pressed="true">Link</a>'+value.manufacture+' | <span class="text-muted">'+value.Model+'</span></li>');
-                  $('#result').append('<li class="list-group-item link-class" id="'+value.Model+'"><button href= class="btn btn-secondary btn-lg" aria-pressed="true">'+value.manufacture+'</button></li>');
+                  $('#result').append('<li class="list-group-item link-class"><button id="'+value.Model+'" class="btn btn-secondary btn-lg" aria-pressed="true">'+value.manufacture+'</button></li>');
 
 
 
@@ -57,7 +57,7 @@ $(document).ready(function() {
 // alert($('#result').manufacture);
 document.getElementById("result").addEventListener("click", function(e){
 
-  if(e.target && e.target.nodeName == "LI"){
+  if(e.target && e.target.nodeName == "BUTTON"){
     alert(e.target.id + "was clicked");
 
   }
