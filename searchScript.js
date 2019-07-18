8e8f76afb4e0e6d7ca3b0c60993879f9af7e8716
David Muniz <david.munizmacias@pop.belmont.edu>
2019-07-18 00:05:32 -0500

    Update searchScript.js

--- searchScript.js
+++ searchScript.js
@@ -67,7 +67,8 @@ document.getElementById("result").addEventListener("click", function(clickFuncti
 
 
 
-  }
+  };
+});
 
 
 
@@ -76,7 +77,7 @@ document.getElementById("result").addEventListener("click", function(clickFuncti
 
 
 
-
+  $(document).ready(function() {
     // import { itemClicked } from "https://projectcure.github.io/searchScript.js"
     // import {clickFunction, itemClicked} from "https://projectcure.github.io/searchScript.js";
     // console.log( itemClicked );
@@ -141,7 +142,7 @@ document.getElementById("result").addEventListener("click", function(clickFuncti
 
 
     });
-
+  });
 
 
 
