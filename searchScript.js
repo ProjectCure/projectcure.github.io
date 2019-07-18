09f017664816a1353d43ae2836e2a4e9a8d11cdb
David Muniz <david.munizmacias@pop.belmont.edu>
2019-07-17 19:43:53 -0500

    Update searchScript.js

--- searchScript.js
+++ searchScript.js
@@ -62,7 +62,7 @@ document.getElementById("result").addEventListener("click", function(clickFuncti
 
   if(clickFunction.target && clickFunction.target.nodeName == "BUTTON"){
     var itemClicked = clickFunction.target.id;
-    module.exports = { variableName: itemClicked };
+    Module.exports = { variableName: itemClicked };
     //export { itemClicked };
     // alert(e.target.id + "was clicked");
     alert(itemClicked);
