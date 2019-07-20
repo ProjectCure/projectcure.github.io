bc7f94eb13979f3128410463bb0f820215c25704
David Muniz <david.munizmacias@pop.belmont.edu>
2019-07-20 02:22:49 -0500

    Update searchScript.js

--- searchScript.js
+++ searchScript.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
         $('#result').html('');
         var searchField = $('#search').val();
         var expression = new RegExp(searchField, "i");
-        $.getJSON('data.json',function(data) {
+        $.getJSON('model.json',function(data) {
             $.each(data, function(key, value) {
               //search bias
                 if (value.manufacture.search(expression) != -1 || value.Model.search(expression) != -1)
@@ -94,7 +94,7 @@ document.getElementById("result").addEventListener("click", function(clickFuncti
 
 
 
-    
+
 
 
     // module.exports.variableName = "variableValue";
